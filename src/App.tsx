c85fd253b0c3a444a9789470d4cba269c6793b39
João Pedro <jp871157@gmail.com>
2024-03-07 23:36:41 -0300

    feat: about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header/Header"
+import { AboutSection } from "./components/Sections/AboutSection/AboutSection"
 import './styles/index.scss'
 
 export const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <AboutSection />
+      </main>
     </>
   )
 }
